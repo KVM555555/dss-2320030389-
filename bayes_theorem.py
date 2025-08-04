976c5c4ac7250c012da77ddfd9173eef6bcda213
KVM555555 <2320030389@klh.edu.in>
2025-08-04 08:34:42 +0530

    Delete bayes_theorem.py

--- bayes_theorem.py
+++
@@ -1,9 +0,0 @@
-def bayes_theorem(p_b_given_a, p_a, p_b):
-    return (p_b_given_a * p_a) / p_b
-
-p_disease = 0.01
-p_positive_given_disease = 0.99
-p_positive = 0.05
-
-p_disease_given_positive = bayes_theorem(p_positive_given_disease, p_disease, p_positive)
-print(f"P(Disease | Positive Test): {round(p_disease_given_positive, 3)}")
